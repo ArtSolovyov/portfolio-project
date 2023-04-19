37a7d6d5ba3ae145344d96b0572917fb66bd53e8
ortyr <arsolovyev@sberbank.ru>
2023-04-19 21:53:51 +0300

    Добавлена структура и корневая страница

---
+++ index.js
@@ -0,0 +1,12 @@
+function setButtonBehavior(selector, event, callback) {
+    const elem = document.querySelector(selector);
+    elem.addEventListener(event, callback);
+}
+
+function setAlert() {
+    alert('Обработка клика!');
+}
+
+setButtonBehavior('.mainPage', 'click', setAlert);
+setButtonBehavior('.projects', 'click', setAlert);
+setButtonBehavior('.contacts', 'click', setAlert);
