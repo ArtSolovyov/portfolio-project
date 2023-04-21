debe4b91554ad16b795d7838a18afecc6667088b
Виталий Михайлов <vitalijmihajlov@MacBook-Air-Vitalij.local>
2023-04-21 14:48:46 +0300

    мой первый коммит

---
+++ portfolio - project/script.js
@@ -0,0 +1,9 @@
+class Human {
+    constructor( age, name, nationality, languages, profession) {
+        this.age = age,
+        this.name = name,
+        this.nationality = nationality,
+        this.languages = languages,
+        this.profession = profession
+    };
+}
